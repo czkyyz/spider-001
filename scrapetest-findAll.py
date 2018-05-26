016dfe3f5ac0b30eea4d401a14ec6e9ed9a64303
czkyyz <39127264+czkyyz@users.noreply.github.com>
2018-05-26 23:50:36 +0800

    Delete scrapetest-findAll.py

--- scrapetest-findAll.py
+++
@@ -1,8 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-
-html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")                 #网页在服务器上不存在
-bsObj = BeautifulSoup(html.read(),'html.parser')
-namelist = bsObj.findAll("span",{"class","green"})
-for name in namelist:
-    print(name.get_text())
